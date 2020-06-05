dd3eefa8facc5b9b228b63629f3867be79353f12
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2020-06-06 01:54:29 +0200

    Initial revision

---
+++ README.md
@@ -0,0 +1,124 @@
+git-runstats
+============
+
+Displays git contribution statistics (insertions + deletions)
+
+Usage
+-----
+
+```
+Usage: git-runstats [OPTIONS] [GITARGS]...
+
+  Most arguments of `git log` will work as GITARGS, but do not change the
+  output-format.
+
+Options:
+  -l, --limit INTEGER  Number of commits to read
+  --tty / --no-tty     Enable tty
+```
+
+Live-stats
+
+```bash
+git runstats
+```
+
+Stats for `README.md`
+
+```bash
+git runstats README.md
+```
+
+Display help
+
+```bash
+git-runstats --help
+```
+
+Non-live stats with limit
+
+```bash
+git runstats -l 1000 | less
+```
+
+Non-live stats revesed
+
+```bash
+git runstats -l 1000 | head -n -1 | sort -n
+```
+
+Show stats in branch
+
+```bash
+git runstats master..my_branch
+```
+
+Show stats in current branch
+
+```bash
+git runstats master..
+```
+
+Show stats from 2019
+
+```bash
+git runstats -- --since=2019-01-01 --until=2019-12-31
+```
+
+Who knows most about the rust Alpine Linux pacakge
+
+```bash
+cd aports
+git runstats community/rust
+```
+
+Install
+-------
+
+```bash
+pip install git-runstats
+```
+
+Why
+---
+
+In comparision to shortlog runstats gives immediate feedback and counts changes
+instead of commits. Press Ctrl-C once you have enough information. Shortlog can
+also be very wrong:
+
+```bash
+$> cd linux
+$> git shortlog -s -n net/802/ | head -n 10
+    11  Stephen Hemminger
+    10  Arnaldo Carvalho de Melo
+     9  Eric Dumazet
+     6  Eric W. Biederman
+     5  Alexey Dobriyan
+     5  David S. Miller
+     4  Paul Gortmaker
+     4  David Ward
+     3  Linus Torvalds
+     3  Adrian Bunk
+```
+
+versus
+
+```bash
+$> cd linux
+$> git runstats net/802/ | head -n 10
+      1556  Linus Torvalds
+       931  David Ward
+       749  Patrick McHardy
+       104  Stephen Hemminger
+        98  Eric Dumazet
+        65  Pavel Emelyanov
+        51  Thomas Gleixner
+        47  Johannes Berg
+        40  Alexey Dobriyan
+        30  Joe Perches
+```
+
+Demo
+----
+
+[![asciicast](https://asciinema.org/a/6YV1oxGoJy5YSTf3qXCHpxqsh.png)](https://asciinema.org/a/6YV1oxGoJy5YSTf3qXCHpxqsh)
---
+++ git_runstats.py
@@ -0,0 +1,177 @@
+import os
+import re
+import shutil
+import signal
+import sys
+import time
+from subprocess import PIPE, CalledProcessError, Popen, check_call
+
+import click
+
+cmd = (
+    "git log --ignore-blank-lines --ignore-all-space " "--no-merges --shortstat"
+).split(" ") + ["--pretty=tformat:next: %aN\u25CF%ai",]
+
+stats_line = re.compile(
+    r"(\d+)\s*files? changed,\s*(\d+)\s*insertions?\(\+\)"
+    r"(,\s*(\d+)\s*deletions?\(\-\))?"
+)
+
+_running = True
+
+
+class EndOfFile(Exception):
+    pass
+
+
+def exit_handler(signum, frame):
+    global _running
+    _running = False
+
+
+signal.signal(signal.SIGINT, exit_handler)
+
+
+class Stats:
+    __slots__ = ("author", "files", "insertions", "deletetions", "sum")
+
+    def __init__(self, author):
+        self.author = author
+        self.files = 0
+        self.insertions = 0
+        self.deletetions = 0
+        self.sum = 0
+
+    def __repr__(self):
+        return f"{self.sum:10}  {self.author}"
+
+
+def readline(stream):
+    if not (line := stream.readline()):
+        raise EndOfFile()
+    return line.strip().decode("UTF-8")
+
+
+def process(stream, stats):
+    author = None
+    current = None
+    while (line := readline(stream)) is not None:
+        if line.startswith("next: "):
+            _, _, author = line.partition("next: ")
+            author, author_date = author.split("\u25CF")
+            author = " ".join([x for x in author.split(" ") if x])
+        elif match := stats_line.match(line):
+            current = stats.get(author)
+            if current is None:
+                current = Stats(author)
+                stats[author] = current
+            files, insertions, _, deletetions = match.groups("0")
+            files = int(files)
+            insertions = int(insertions)
+            deletetions = int(deletetions)
+            current.files += files
+            current.insertions += insertions
+            current.deletetions += deletetions
+            current.sum += insertions + deletetions
+            break
+    return author_date
+
+
+def output(stats, commits, date, max=None):
+    rows = sorted(stats.items(), key=lambda x: x[1].sum, reverse=True)
+    if max is not None:
+        rows = rows[:max]
+    for item in rows:
+        print(item[1])
+    print(f"\n{commits:10}  commits ({date})")
+
+
+def term_output(stats, commits, date, max):
+    rows = sorted(stats.items(), key=lambda x: x[1].sum, reverse=True)[:max]
+    for item in rows:
+        sys.stdout.write("\033[K")
+        print(item[1])
+    sys.stdout.write("\033[K\n\033[K")
+    sys.stdout.write(f"{commits:10} commits ({date})")
+    sys.stdout.flush()
+
+
+def display(stamp, stats, commits, date):
+    global _outputs
+    new = time.monotonic()
+    if new - stamp > 0.2:
+        size = shutil.get_terminal_size((80, 20))
+        lines = min(25, size.lines - 2)
+        sys.stdout.write("\033[0;0f")
+        term_output(stats, commits, date, lines)
+        lines = size.lines - 6
+        return new, lines
+    return stamp, None
+
+
+def processing(limit, isatty, proc):
+    date = None
+    lines = None
+    commits = 0
+    stats = {}
+    stream = proc.stdout
+    try:
+        if isatty:
+            sys.stdout.write("\033[H\033[J")
+            stamp = time.monotonic() - 0.1
+            while _running and (not limit or commits < limit):
+                date = process(stream, stats)
+                stamp, lines_update = display(stamp, stats, commits, date)
+                if lines_update:
+                    lines = lines_update
+                commits += 1
+        else:
+            while _running and (not limit or commits < limit):
+                date = process(stream, stats)
+                commits += 1
+        signal.signal(signal.SIGINT, signal.default_int_handler)
+    except EndOfFile:
+        pass
+    finally:
+        if date:
+            if isatty and commits:
+                sys.stdout.write("\033[H\033[J")
+            output(stats, commits, date, lines)
+            if not _running:
+                sys.stdout.write("\033[0A")
+
+
+@click.command(
+    help=(
+        "Display git contribution statistics (insertions + deletions). "
+        "Most arguments of `git log` will work as GITARGS, but do not change "
+        "the output-format."
+    )
+)
+@click.option("-l", "--limit", default=0, type=int, help="Number of commits to read")
+@click.option("--tty/--no-tty", default=True, help="Enable tty")
+@click.argument("gitargs", nargs=-1)
+def main(limit, gitargs, tty):
+    ext = []
+    if gitargs:
+        ext = list(gitargs)
+    try:
+        test = ext
+        if "-n" in ext:
+            pos = ext.index("-n")
+            test = ext[:pos] + ext[pos + 2 :]
+        check_call(cmd + ["-n", "0"] + test)
+    except CalledProcessError:
+        raise click.Abort()
+    isatty = tty and os.isatty(sys.stdout.fileno())
+    proc = Popen(cmd + ext, stdout=PIPE, preexec_fn=os.setsid)
+    try:
+        processing(limit, isatty, proc)
+    finally:
+        proc.terminate()
+        if not proc.wait(1):
+            proc.kill()
+
+
+if __name__ == "__main__":
+    main()
